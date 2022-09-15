f5f9f72a89b59937170faf6b4a79d7ed314545b1
magicteo <matty.macaluso@gmail.com>
2022-09-15 12:05:16 +0200

    n.14

--- leetcode/14_Longest_Common_Prefix.py
+++ leetcode/14_Longest_Common_Prefix.py
@@ -5,5 +5,7 @@ class Solution:
         for i in range(len(strs[0])):
             for s in strs:
                 if i == len(s) or s[i] != strs[0][i]:
-                    return res
-            res += strs[0][i]
+                    return result
+            result += strs[0][i]
+
+        return result
