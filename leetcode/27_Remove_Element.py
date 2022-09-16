48fe092282b740556a925bed3b58ef8188fd8802
magicteo <matty.macaluso@gmail.com>
2022-09-16 12:42:11 +0200

    .

--- leetcode/27_Remove_Element.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        
-        l = len(nums)
-
-        for i in range(l):
-            if nums[i] == val:
-                nums
