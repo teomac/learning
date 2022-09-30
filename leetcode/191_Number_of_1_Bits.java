f35c2638755f031591a1a7055b7b8d3100f22c58
magicteo <matty.macaluso@gmail.com>
2022-09-30 12:41:26 +0200

    n.191

---
+++ leetcode/191_Number_of_1_Bits.java
@@ -0,0 +1,21 @@
+class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+
+        String s = Integer.toBinaryString(n);
+        int l = s.length();
+        int counter = 0;
+
+        for(int i=0; i<l; i++){
+            if(s.charAt(i) == '1'){
+                counter++;
+            }
+            else{
+                continue;
+            }
+        }
+
+        return counter;
+        
+    }
+}
