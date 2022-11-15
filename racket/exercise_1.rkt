4888f682c78358062737f474ad7274748b41fa01
magicteo <matty.macaluso@gmail.com>
2022-11-15 18:10:14 +0100

    updated exercise_1

--- racket/exercise_1.rkt
+++ racket/exercise_1.rkt
@@ -11,3 +11,20 @@
 (define (prefix n L)
   (if (= n 0) '()
       (cons (car L) (prefix (- n 1) (cdr L)))))
+
+; implementation of vector-ref for lists
+(define (ref k L)
+  (if (= k 0) (car L)
+        (ref (- k 1) (cdr L))))
+
+; range function
+; range(3) --> (0 1 2 3)
+; range(2 4) --> (2 3 4)
+(define (range s . e)
+  (define (r s e)
+    (if (= s e)
+        (list s)
+        (cons s (r (+ s 1) e))))
+  (if (null? e)
+      (r 0 s)
+      (r s (car e))))
