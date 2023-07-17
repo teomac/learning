e90d6bdfcc5cfa9d1afa54cb291ec6397cc96be2
magicteo <matty.macaluso@gmail.com>
2023-07-17 16:47:24 +0200

    added smtp

--- movies_notifier/movies_script.py
+++ movies_notifier/movies_script.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-output = 'Movies of the week:\n'
+from email.message import EmailMessage
+import smtplib
+
+output = 'Movies of the week:\n\n'
 
 url = "https://www.cinelandia.it/le-nostre-sale/cinema-como.html"
 page = requests.get(url)
@@ -19,3 +22,19 @@ for text in filtered_text2:
     output += text + '\n'
 
 print(output)
+
+sender = "pythonproj5@outlook.com"
+recipient = "andreaportaliwings@gmail.com"
+message = output
+
+email = EmailMessage()
+email["From"] = sender
+email["To"] = recipient
+email["Subject"] = "Movies of the week TEST"
+email.set_content(message)
+
+smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
+smtp.starttls()
+smtp.login(sender, "password")
+smtp.sendmail(sender, recipient, email.as_string())
+smtp.quit()
